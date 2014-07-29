1916ffd7d5cdcd0aaf99b7790021c149f2f2c676
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-07-28 23:41:09 -0300

    ByOptions and searchcontext suport

--- src/main/java/com/jprotractor/NgBy.java
+++ src/main/java/com/jprotractor/NgBy.java
@@ -5,13 +5,18 @@ import org.openqa.selenium.WebDriver;
 
 import com.jprotractor.bys.ByBinding;
 import com.jprotractor.bys.ByModel;
+import com.jprotractor.bys.ByOptions;
 
-public final class NgBy {
+public abstract class NgBy extends By {
 	public static By model(final String model) {
 		return new ByModel(model);
 	}
 
 	public static By binding(final String model, final WebDriver driver) {
 		return new ByBinding(driver, model);
+	}
+
+	public static By options(final String options, final WebDriver driver) {
+		return new ByOptions(options, driver);
 	}
 }
