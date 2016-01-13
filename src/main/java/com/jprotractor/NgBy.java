9129095f36701b81af72021f5a8ea34c44393b08
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-12 23:05:57 -0200

    Revert "starting to improve code"
    This reverts commit 539bd4eb753dd9c7eb9adeba94321ba95df7fa4d.

--- src/main/java/com/jprotractor/NgBy.java
+++ src/main/java/com/jprotractor/NgBy.java
@@ -3,43 +3,42 @@ package com.jprotractor;
 import org.openqa.selenium.By;
 //import com.ostusa.JavaScriptBy;
 
-public class NgBy {
-    private NgBy() {
-    }
+public class NgBy 
+{
+	private NgBy() { }
+	
+	public static By binding(String binding){
+		return new JavaScriptBy(ClientSideScripts.FindBindings, binding);
+	}
+	
+	public static By buttonText(String text){
+		return new JavaScriptBy(ClientSideScripts.FindButtonText, text);
+	}
+	
+	public static By input(String input){
+		return new JavaScriptBy(ClientSideScripts.FindModel, input);
+	}
 
-    public static By binding(String binding) {
-        return new JavaScriptBy(ClientSideScripts.FindBindings, binding);
-    }
+	public static By model(String model){
+		return new JavaScriptBy(ClientSideScripts.FindModel, model);
+	}
 
-    public static By buttonText(String text) {
-        return new JavaScriptBy(ClientSideScripts.FindButtonText, text);
-    }
+	public static By partialButtonText(String text){
+		return new JavaScriptBy(ClientSideScripts.PartialButtonText, text);
+	}
+	
+	public static By repeater(String repeat){
+		return new JavaScriptBy(ClientSideScripts.FindAllRepeaterRows, repeat);
+	}
+	public static By repeaterColumn(String repeat, String binding){
+		return new JavaScriptBy(ClientSideScripts.FindRepeaterColumn, repeat, binding);
+	}
 
-    public static By input(String input) {
-        return new JavaScriptBy(ClientSideScripts.FindModel, input);
-    }
-
-    public static By model(String model) {
-        return new JavaScriptBy(ClientSideScripts.FindModel, model);
-    }
-
-    public static By partialButtonText(String text) {
-        return new JavaScriptBy(ClientSideScripts.PartialButtonText, text);
-    }
-
-    public static By repeater(String repeat) {
-        return new JavaScriptBy(ClientSideScripts.FindAllRepeaterRows, repeat);
-    }
-
-    public static By repeaterColumn(String repeat, String binding) {
-        return new JavaScriptBy(ClientSideScripts.FindRepeaterColumn, repeat, binding);
-    }
-
-    public static By options(String options) {
-        return new JavaScriptBy(ClientSideScripts.FindOptions, options);
-    }
-
-    public static By selectedOption(String model) {
-        return new JavaScriptBy(ClientSideScripts.FindSelectedOption, model);
-    }
+	public static By options(String options){
+		return new JavaScriptBy(ClientSideScripts.FindOptions, options);
+	}
+	
+	public static By selectedOption(String model){
+		return new JavaScriptBy(ClientSideScripts.FindSelectedOption, model);
+	}
 }
